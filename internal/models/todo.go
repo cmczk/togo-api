f8ab51102f74f0ce838e32b3a3607a393a5fb2ad
Yauheni Kuntsevich <cmaczok@gmail.com>
2026-02-12 20:57:09 +0300

    feat: add todo models

---
+++ internal/models/todo.go
@@ -0,0 +1,24 @@
+package models
+
+import "time"
+
+type Todo struct {
+	ID          int       `json:"id" db:"id"`
+	Title       string    `json:"title" db:"title"`
+	Description string    `json:"description" db:"desctiption"`
+	Completed   bool      `json:"completed" db:"completed"`
+	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt   time.Time `json:"updatedAt" db:"updated_at"`
+}
+
+type CreateTodoInput struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
+
+type UpdateTodoInput struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	Completed   *bool   `json:"completed"`
+}
