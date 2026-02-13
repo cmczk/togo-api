00206beffc1575fccb4c32d4f51251e67db22ff9
Yauheni Kuntsevich <cmaczok@gmail.com>
2026-02-13 09:55:26 +0300

    feat: add get by id, create, update & delete storage methods

--- internal/database/todos.go
+++ internal/database/todos.go
@@ -1,7 +1,10 @@
 package database
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/cmczk/todo-api/internal/models"
 	"github.com/jmoiron/sqlx"
@@ -30,4 +33,94 @@ func (s *TodoStore) GetAll() (todos []models.Todo, err error) {
 	}
 
 	return todos, nil
+}
+
+func (s *TodoStore) GetByID(id int) (todo *models.Todo, err error) {
+	const op = "database.todos.GetByID"
+
+	query := `
+	SELECT
+		id, title, description,
+		created_at, updated_at
+	FROM todos
+	WHERE id = $1;`
+
+	if err := s.db.Get(todo, query, id); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("todo not found")
+		}
+
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return todo, nil
+}
+
+func (s *TodoStore) Create(data models.CreateTodoInput) (*models.Todo, error) {
+	const op = "database.todos.Create"
+
+	var todo models.Todo
+
+	query := `
+	INSERT INTO
+		todos (title, description, completed)
+	VALUES ($1, $2, $3)
+	RETURNING id, title, description, created_at, updated_at;`
+
+	if err := s.db.QueryRowx(
+		query, data.Title, data.Description, data.Completed,
+	).StructScan(&todo); err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &todo, nil
+}
+
+func (s *TodoStore) Update(id int, data models.UpdateTodoInput) (*models.Todo, error) {
+	const op = "database.todos.Update"
+
+	todo, err := s.GetByID(id)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	if data.Title != nil {
+		todo.Title = *data.Title
+	}
+
+	if data.Description != nil {
+		todo.Description = *data.Description
+	}
+
+	if data.Completed != nil {
+		todo.Completed = *data.Completed
+	}
+
+	query := `
+	UPDATE todos
+	SET title = $1, description = $2, completed = $3, updated_at = $4
+	WHERE id = $5
+	RETURNING id, title, description, created_at, updated_at;`
+
+	var updatedTodo models.Todo
+
+	if err := s.db.QueryRowx(
+		query, todo.Title, todo.Description, todo.Completed, time.Now(), id,
+	).StructScan(&updatedTodo); err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &updatedTodo, nil
+}
+
+func (s *TodoStore) Delete(id int) error {
+	const op = "database.todos.Delete"
+
+	query := "DELETE FROM todos WHERE id = $1;"
+
+	if _, err := s.db.Exec(query, id); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
